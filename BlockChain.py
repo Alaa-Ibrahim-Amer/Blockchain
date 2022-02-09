f31ff432d76afdf0d7859ca249f0e8c1935889c5
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2022-02-09 02:33:01 +0200

    add miners

--- BlockChain.py
+++ BlockChain.py
@@ -20,8 +20,7 @@ class Blockchain:
         self.chain = []
         self.create_genesis_block()
         self.difficulty=difficulty 
-        self.branch = []
- 
+       
     def create_genesis_block(self):
         genesis_block = Block(0, [], time.time(), "0")
         genesis_block.hash = genesis_block.compute_hash()
@@ -75,7 +74,100 @@ class Blockchain:
         self.add_block(new_block, proof)
         self.unconfirmed_transactions = []
         return new_block.index
+    def check_chain_validity(cls, chain):
+        
+        result = True
+        previous_hash = "0"
 
+        # Iterate through every block
+        for block in chain:
+            block_hash = block.hash
+            # remove the hash field to recompute the hash again
+            # using `compute_hash` method.
+            delattr(block, "hash")
+
+            if not cls.is_valid_proof(block, block.hash) or \
+                    previous_hash != block.previous_hash:
+                result = False
+                break
+
+            block.hash, previous_hash = block_hash, block_hash
+
+        return result
+
+class peer:
+    def __init__(self,basechain):
+        self.basechain = basechain
+        self.unconfirmed_transactions = basechain.unconfirmed_transactions
+        
+    def add_transaction(self,unconfirmed_transaction):
+        self.basechain.add_new_transaction(unconfirmed_transaction)
+       
+
+    def miner(self):
+        self.basechain.mine()
+    
+    def broadcast(self,BlockChain):
+        self.basechain = BlockChain
+        self.unconfirmed_transactions = BlockChain.unconfirmed_transactions
+        
+
+class system:
+     def __init__(self,Blockchain):
+         self.Blockchain = Blockchain
+         self.miners = []
+
+     def choose_longest_chain(self):
+        longest_chain = None
+        current_len = len(self.Blockchain.chain)
+       
+        for m in self.miners:
+
+            if len(m.basechain.chain) > current_len and m.basechain.check_chain_validity():
+                # Longer valid chain found!
+                current_len = len(m.basechain.chain)
+                longest_chain = m.basechain
+                print('1')
+            print('0')
+          
+        if longest_chain:
+            self.Blockchain = longest_chain
+            print(json.dumps(longest_chain.__dict__))
+            return True
+
+        return False   
+     def Broadcast(self):
+        self.choose_longest_chain()
+        for m in self.miners:
+            m.broadcast(self.Blockchain)
+          
+
+    
+b=Blockchain(int(4))
+b.add_new_transaction('alice pay bob 100')
+
+s=system(b)
+miner1=peer(s.Blockchain)
+s.miners.append(miner1)
+
+miner2=peer(s.Blockchain)
+s.miners.append(miner2)
+
+s.miners[0].miner()
+print('########## miner 1 ##########\t\t')
+block_string = json.dumps(s.miners[0].basechain.chain[1].__dict__)
+print(block_string)
+
+#s.Broadcast()
+
+print('########## miner2 ##########\t\t')
+block_string = json.dumps(s.miners[1].basechain.chain[1].__dict__)
+print(block_string)
+
+s.choose_longest_chain()
+
+    
+'''
 d = input("add diffculty: ")
 
 chain=Blockchain(int(d))
@@ -86,7 +178,7 @@ for i in range (10):
     block_string = json.dumps(chain.chain[i].__dict__, sort_keys=True)
     print(block_string)
    
-   
+'''
 
 
 
